ae134e86f81167c5e399adc71188db20bf9e8971
Jessica Bradham <59948120+jessijoke@users.noreply.github.com>
2021-08-21 03:45:37 +0000

    Array Algorithms and Big O

--- datastruct.js
+++ datastruct.js
@@ -1,1 +1,13 @@
-console.log('test');
+const strings = ["a", "b", "c", "d"];
+
+console.log(strings[2]);
+
+strings.push('e'); //O(1)
+strings.pop();//O(1)
+strings.pop();
+
+strings.unshift('x'); //O(n)
+
+strings.splice(2, 0, "alien"); //O(n)
+
+console.log(strings);
